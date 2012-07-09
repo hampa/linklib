e0a1bdf6d8a14d87b3d1eb8461a90e52587517f5
hampa <hampus@nabistudios.com>
2012-07-09 14:26:32 +0000

    removed movie viewing from testapi

--- testapi.php
+++ testapi.php
@@ -25,35 +25,6 @@ if ($timefeedid > 0) {
 <script src="timefeed.js"></script>
 <link href="css/timefeed.css" rel="stylesheet" />
 <link href="css/apistyle.css" rel="stylesheet" />
-<script>
-        // ensure the web page (DOM) has loaded
-        document.addEventListener("DOMContentLoaded", function () {
-
-            timefeed('#feeddiv', {
-                highlight: true
-            });
-
-            // Create a popcorn instance by calling the Youtube player plugin
-            //popcorn = Popcorn("#video");
-	    popcorn = Popcorn.youtube("#youtube", "http://www.youtube.com/watch?v=v-7kf7OZQtw");
-            popcorn.volume(0);
-<?php
-$linkontrol = new linkontrol();
-$arr = $linkontrol->getTimeFeed($movieid);
-if (isset($arr)) {
-	foreach ($arr as $key => $val) {
-		echo($linkontrol->timeFeedToJson($val));
-	}
-}
-?>
-            // initial render
-            //toggleOverlay(true, true);
-
-            // play
-            popcorn.play();
-        }, false);
-
-</script>
 </head>
 <body>
 <div>
@@ -78,7 +49,7 @@ if (isset($arr)) {
 <form method=post action="http://linkontrol.toribash.com/~hampa/testapi.php?do=add_movie" >
 <table>
 <tr><td>userid:</td>	<td><input name="userid" value="2101483"></td></tr>
-<tr><td>name:</td>	<td><input size="60" name="body" value="Prometheus"></td></tr>
+<tr><td>name:</td>	<td><input size="60" name="name" value="Prometheus"></td></tr>
 <tr><td>href:</td>	<td><input size="60" name="href" value="http://en.wikipedia.org/wiki/"></td></tr>
 <tr><td></td><td><input type=submit value="Submit"></td></tr>
 </table>
@@ -150,26 +121,5 @@ if (isset($arr)) {
 <div class=success>This is a success message</div>
 <div class=warning>This is an warning message</div>
 <div class=error>This is an error message</div>
-
-<div class="container">
-<!--
-    	<video height=100 width=100 style="background:#000" id="video" preload="auto" autobuffer="" controls="" poster="http://videos.mozilla.org/serv/webmademovies/popcornposter.png">
-        <source src="http://videos.mozilla.org/serv/webmademovies/wtfpopcorn.mp4" type="video/mp4">
-        <source src="http://videos.mozilla.org/serv/webmademovies/wtfpopcorn.webm" type="video/webM">
-        <source src="http://videos.mozilla.org/serv/webmademovies/wtfpopcorn.ogv" type="video/ogg">
-        <p>Your user agent does not support the HTML5 Video element.</p>
-    </video>
--->
-    <!--<div id="viddeo">dum-->
-    <!--</div>-->
-    <div class="feed">
-        <div class="searchdiv">
-            <input type=text class="search" placeholder="Search">
-        </div>
-        <div id="feeddiv">
-    </div>
-</div>
-<div id="youtube" style="width:600px;height:400px;"></div>
-<div id="foo"></div>
 </body>
 </html>
