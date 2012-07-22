8ec98c4b10ac58fc6a3f390b367b681483da9c5b
hampa <hampus@nabistudios.com>
2012-07-22 21:48:33 +0000

    fixed db connection

--- linkontrol/global.php
+++ linkontrol/global.php
@@ -5,8 +5,8 @@ define('GLOBAL', true);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'linkontrol');
 define('DB_USER', 'root');
-#define('DB_PASS', 'mANd4p1Zda');
-define('DB_PASS', '');
+define('DB_PASS', 'mANd4p1Zda');
+#define('DB_PASS', '');
 
 #define('TP_PATH', 'templates/');
 #define('TP_EXTN', '.html');
